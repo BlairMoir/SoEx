83ed59e7fbdaa99afd5072e203bd0f837909ac39
Blair Moir <blairmoir@gmail.com>
2024-09-22 07:23:46 +0100

    SoEx Proxy

---
+++ Library/Proxy/Proxy.cs
@@ -0,0 +1,21 @@
+namespace SoEx
+{
+    public static class Proxy
+    {
+        public static I ForService<I>() where I : class
+        {
+            return CreateProxy<I>();
+        }
+
+        public static I ForComponent<I>(object service) where I : class
+        {
+            ArgumentNullException.ThrowIfNull(service);
+            return CreateProxy<I>();
+        }
+
+        private static I CreateProxy<I>() where I : class
+        {
+            return Container.Resolve<I>();
+        }
+    }
+}
