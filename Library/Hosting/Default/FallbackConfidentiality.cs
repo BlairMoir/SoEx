adba0510a419a71c6c408d7406e1309e4b174de1
Blair Moir <blairmoir@gmail.com>
2026-06-15 12:07:08 +0100

    Fix rogue characters in FallbackConfidentiality

--- Library/Hosting/Default/FallbackConfidentiality.cs
+++ Library/Hosting/Default/FallbackConfidentiality.cs
@@ -5,6 +5,6 @@ namespace SoEx.Hosting.Default
     public class FallbackConfidentiality : ITelemetryConfidentiality
     {
         public string Protect(string message) => "[redacted]";
-        public string Protect<T>(T message) => $"[redacted:{message?.GetType().Name ?? typeof(T).Name}])]";
+        public string Protect<T>(T message) => $"[redacted:{message?.GetType().Name ?? typeof(T).Name}]";
     }
 }
