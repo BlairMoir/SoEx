bc98224786b4f09a995028f3d2aa5cd8de7a3c61
Blair Moir <blairmoir@gmail.com>
2026-09-02 15:23:07 +0100

    Keep derived types in System.Text.Json Serializer

--- Library/Hosting/Serializers/SystemText/JsonMessageSerializer.cs
+++ Library/Hosting/Serializers/SystemText/JsonMessageSerializer.cs
@@ -1,4 +1,6 @@
 using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
 using SoEx.Abstractions;
 using SoEx.Hosting.Serializers.Common;
 
@@ -11,7 +13,35 @@ namespace SoEx.Hosting.Serializers.SystemText
         public JsonMessageSerializer(KnownTypes knownTypes)
         {
             _options = new JsonSerializerOptions();
+            _options.TypeInfoResolver = new DefaultJsonTypeInfoResolver()
+            {
+                Modifiers = { info => ApplyPolymorphism(knownTypes, info) },
+            };
             _options.Converters.Add(new KnownTypeObjectConverter(new KnownTypeRegistry(knownTypes.Types)));
+        }
+
+        private void ApplyPolymorphism(KnownTypes knownTypes, JsonTypeInfo info)
+        {
+            if (info.Kind != JsonTypeInfoKind.Object)
+                return;
+
+            var types = knownTypes.Types.ToArray();
+            var derived = types.Where( t=> t != info.Type && info.Type.IsAssignableFrom(t)).ToArray();
+
+            if (derived.Length == 0)
+                return;
+
+            info.PolymorphismOptions = new JsonPolymorphismOptions()
+            {
+                TypeDiscriminatorPropertyName = "$type",
+                IgnoreUnrecognizedTypeDiscriminators = false,
+                UnknownDerivedTypeHandling = JsonUnknownDerivedTypeHandling.FailSerialization
+            };
+
+            foreach (var type in derived)
+            {
+                info.PolymorphismOptions.DerivedTypes.Add(new JsonDerivedType(type,type.FullName!));
+            }
         }
 
         public T? Deserialize<T>(byte[] payload)
